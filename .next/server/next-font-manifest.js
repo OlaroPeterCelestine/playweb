abfe79ca8fa4a9dc02895771635a274c7e53896d
PETER Celestine OLARO <olaropetercelestine@gmail.com>
2025-11-18 04:00:41 +0300

    Add .next to .gitignore

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/olaropetercelestine/Desktop/october/assignments-fixed/play/Untitled/app/layout\":[\"static/media/e4af272ccee01ff0-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
